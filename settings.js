5ffc86a829fc44f397ab2d07d6a350360da86925
Sarah <frejulian57@gmail.com>
2026-01-14 22:11:57 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,31 +6,36 @@ global.botNumber = ''
 global.sessionName = 'Sessions/Owner'
 global.version = '^2.0 - Latest'
 global.dev = "© mᥲძᥱ ᥕі𝗍һ ᑲᥡ 𓆩‌۫᷼ ִֶָღ݉͢𝓢𝓪𝓻𝓪𝓱𓆪‌"
+
 global.links = {
-api: 'https://api.stellarwa.xyz',
-channel: "https://whatsapp.com/channel/0029VaCDajZ9WtBvBZy76k2h",
-github: "https://github.com",
-gmail: "danivelasco745@gmail.com"
+  api: 'https://api.stellarwa.xyz',
+  channel: "https://whatsapp.com/channel/0029VaCDajZ9WtBvBZy76k2h",
+  github: "https://github.com",
+  gmail: "danivelasco745@gmail.com"
 }
+
+// 🔹 FOTO / VIDEO DEL MENÚ
+global.banner = 'https://raw.githubusercontent.com/AkiraDevX/uploads/main/uploads/1767704977299_946356.jpeg'
+
 global.my = {
-ch: '120363188537623366@newsletter',
-name: '..૭ 𝐘𝐮𝐦𝐢𝐁𝐨𝐭 - 𝐀𝐈 ৴৴ 𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ⌕',
+  ch: '120363188537623366@newsletter',
+  name: '..૭ 𝐘𝐮𝐦𝐢𝐁𝐨𝐭 - 𝐀𝐈 ৴৴ 𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ⌕',
 }
 
 global.mess = {
-socket: '《✧》 Este comando solo puede ser ejecutado por un Socket.',
-admin: '《✧》 Este comando solo puede ser ejecutado por los Administradores del Grupo.',
-botAdmin: '《✧》 Este comando solo puede ser ejecutado si el Socket es Administrador del Grupo.'
+  socket: '《✧》 Este comando solo puede ser ejecutado por un Socket.',
+  admin: '《✧》 Este comando solo puede ser ejecutado por los Administradores del Grupo.',
+  botAdmin: '《✧》 Este comando solo puede ser ejecutado si el Socket es Administrador del Grupo.'
 }
 
 global.APIs = { 
-vreden: { url: "https://api.vreden.web.id", key: null },
-nekolabs: { url: "https://api.nekolabs.web.id", key: null },
-siputzx: { url: "https://api.siputzx.my.id", key: null },
-delirius: { url: "https://api.delirius.store", key: null },
-ootaizumi: { url: "https://api.ootaizumi.web.id", key: null },
-stellar: { url: "https://api.stellarwa.xyz", key: "Destroy" },
-apifaa: { url: "https://api-faa.my.id", key: null },
-xyro: { url: "https://api.xyro.site", key: null },
-yupra: { url: "https://api.yupra.my.id", key: null }
+  vreden: { url: "https://api.vreden.web.id", key: null },
+  nekolabs: { url: "https://api.nekolabs.web.id", key: null },
+  siputzx: { url: "https://api.siputzx.my.id", key: null },
+  delirius: { url: "https://api.delirius.store", key: null },
+  ootaizumi: { url: "https://api.ootaizumi.web.id", key: null },
+  stellar: { url: "https://api.stellarwa.xyz", key: "Destroy" },
+  apifaa: { url: "https://api-faa.my.id", key: null },
+  xyro: { url: "https://api.xyro.site", key: null },
+  yupra: { url: "https://api.yupra.my.id", key: null }
 }
