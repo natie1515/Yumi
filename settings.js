5f15cce61873ae01bf1935041665d1147a1cf779
Sarah <frejulian57@gmail.com>
2026-01-14 22:27:47 -0400

    Refactor settings.js with new global variables

--- settings.js
+++ settings.js
@@ -1,41 +1,68 @@
-import fs from 'fs';
+import { watchFile, unwatchFile } from "fs"
+import chalk from "chalk"
+import { fileURLToPath } from "url"
+import fs from "fs"
 
-global.owner = ['559296077349', '5492916450307', '5216671548329', '573247662531']
-global.botNumber = ''
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.sessionName = 'Sessions/Owner'
-global.version = '^2.0 - Latest'
-global.dev = "© mᥲძᥱ ᥕі𝗍һ ᑲᥡ 𓆩‌۫᷼ ִֶָღ݉͢𝓢𝓪𝓻𝓪𝓱𓆪‌"
+//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
+//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
+global.botNumber = "" //Ejemplo: 573218138672
 
-global.links = {
-  api: 'https://api.stellarwa.xyz',
-  channel: "https://whatsapp.com/channel/0029VaCDajZ9WtBvBZy76k2h",
-  github: "https://github.com",
-  gmail: "danivelasco745@gmail.com"
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.owner = ["559296077349", "5492916450307", "5216671548329"]
+global.suittag = ["573196588149"] 
+global.prems = []
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.libreria = "Baileys Multi Device"
+global.vs = "^1.8.2|Latest"
+global.sessions = "Sessions/Principal"
+global.jadi = "Sessions/SubBot"
+global.yukiJadibts = true
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.botname = "🌸੭ ׅ 𝐘𝐮𝐦𝐢𝐁𝐨𝐭 - 𝐀𝐈 ׁ ♡ "
+global.textbot = "₊˚ʏᴜᴍɪʙᴏᴛ, mᥲძᥱ ᥕі𝗍һ ᑲᥡ 𓆩‌۫᷼ ִֶָღ݉͢𝓢𝓪𝓻𝓪𝓱𓆪‌ ˙ꨂﾟ"
+global.dev = " ׅ₊˚mᥲძᥱ ᥕі𝗍һ ᑲᥡ 𓆩‌۫᷼ ִֶָღ݉͢𝓢𝓪𝓻𝓪𝓱𓆪‌ ˙"
+global.author = "© mᥲძᥱ ᥕі𝗍һ ᑲᥡ 𓆩‌۫᷼ ִֶָღ݉͢𝓢𝓪𝓻𝓪𝓱𓆪‌"
+global.etiqueta = "✫ 𓆩‌۫᷼ ִֶָღ݉͢𝓢𝓪𝓻𝓪𝓱𓆪‌ ٩(◕‿◕｡)۶"
+global.currency = "¥enes"
+global.banner = "https://raw.githubusercontent.com/AkiraDevX/uploads/main/uploads/1767704977299_946356.jpeg"
+global.icono = "https://raw.githubusercontent.com/AkiraDevX/uploads/main/uploads/1767705260761_580241.jpeg"
+global.catalogo = fs.readFileSync('./lib/catalogo.jpg')
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.group = "https://chat.whatsapp.com/IKY7BgP20J5BYRiRZzINQU"
+global.community = "https://chat.whatsapp.com/FFl5CSUCbBqLVdiRaFVmHp"
+global.channel = "https://whatsapp.com/channel/0029VaCDajZ9WtBvBZy76k2h"
+global.github = "https://github.com"
+global.gmail = "yuminotify.supp@gmail.com"
+global.ch = {
+ch1: "120363188537623366@newsletter"
 }
 
-// 🔹 FOTO / VIDEO DEL MENÚ
-global.banner = 'https://raw.githubusercontent.com/AkiraDevX/uploads/main/uploads/1767704977299_946356.jpeg'
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.my = {
-  ch: '120363188537623366@newsletter',
-  name: '..૭ 𝐘𝐮𝐦𝐢𝐁𝐨𝐭 - 𝐀𝐈 ৴৴ 𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ⌕',
+global.APIs = {
+xyro: { url: "https://api.xyro.site", key: null },
+yupra: { url: "https://api.yupra.my.id", key: null },
+vreden: { url: "https://api.vreden.web.id", key: null },
+delirius: { url: "https://api.delirius.store", key: null },
+zenzxz: { url: "https://api.zenzxz.my.id", key: null },
+siputzx: { url: "https://api.siputzx.my.id", key: null },
+adonix: { url: "https://api-adonix.ultraplus.click", key: 'Yuki-WaBot' }
 }
 
-global.mess = {
-  socket: '《✧》 Este comando solo puede ser ejecutado por un Socket.',
-  admin: '《✧》 Este comando solo puede ser ejecutado por los Administradores del Grupo.',
-  botAdmin: '《✧》 Este comando solo puede ser ejecutado si el Socket es Administrador del Grupo.'
-}
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.APIs = { 
-  vreden: { url: "https://api.vreden.web.id", key: null },
-  nekolabs: { url: "https://api.nekolabs.web.id", key: null },
-  siputzx: { url: "https://api.siputzx.my.id", key: null },
-  delirius: { url: "https://api.delirius.store", key: null },
-  ootaizumi: { url: "https://api.ootaizumi.web.id", key: null },
-  stellar: { url: "https://api.stellarwa.xyz", key: "Destroy" },
-  apifaa: { url: "https://api-faa.my.id", key: null },
-  xyro: { url: "https://api.xyro.site", key: null },
-  yupra: { url: "https://api.yupra.my.id", key: null }
-}
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+unwatchFile(file)
+console.log(chalk.redBright("Update 'settings.js'"))
+import(`${file}?update=${Date.now()}`)
+})
