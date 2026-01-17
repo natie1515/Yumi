d45d0bcdd22fc917c0a22901e08c43f3d0ef19cb
Ell Sarmiento 🦩 <sarmientodieg@gmail.com>
2026-01-17 15:50:13 -0500

    Actualizar subbot.js

--- commands/socket/subbot.js
+++ commands/socket/subbot.js
@@ -1,7 +1,7 @@
 export default {
   command: ['code'],
   category: 'socket',
-  run: async (client, m, args, command) => {
+  run: async (client, m) => {
 
    const rtx = '`✤` Vincula un *sub-bot* a tu *cuenta*\n\nꕤ Importante\n> ₊·( 🜸 ) ➭ El código generado desde la web solo funciona en el mismo número que lo solicitó. No se puede usar en otro número.\n\n> ✥ Para convertirse en *sub-bot*, haz la solicitud únicamente desde la web oficial:\n› *Web* :: ' + `${global.links.web}\n› Solo desde ahí se acepta la solicitud.\n\n> ✥ También puedes cambiar las configuraciones del *sub-bot* desde esa misma web.`;
 
