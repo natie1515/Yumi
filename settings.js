fafaa7cc1e0ac64e705f68b15a7af85aa35f648a
Sarah <frejulian57@gmail.com>
2026-02-08 10:41:55 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,7 +31,7 @@ nekolabs: { url: "https://api.nekolabs.web.id", key: null },
 siputzx: { url: "https://api.siputzx.my.id", key: null },
 delirius: { url: "https://api.delirius.store", key: null },
 ootaizumi: { url: "https://api.ootaizumi.web.id", key: null },
-stellar: { url: "https://api.stellarwa.xyz", key: "nekobot", key2: '1bcd4698ce6c75217275c9607f01fd99' },
+stellar: { url: "https://api.stellarwa.xyz", key: "YukiWaBot", key2: '1bcd4698ce6c75217275c9607f01fd99' },
 apifaa: { url: "https://api-faa.my.id", key: null },
 xyro: { url: "https://api.xyro.site", key: null },
 yupra: { url: "https://api.yupra.my.id", key: null }
