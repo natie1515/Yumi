d63025e50e9b1601d5a9497ad5423a8e2956a679
Sarah <frejulian57@gmail.com>
2026-02-08 17:43:23 -0400

    Update codeprem.js

--- commands/socket/codeprem.js
+++ commands/socket/codeprem.js
@@ -4,20 +4,23 @@ export default {
   command: ['genpremium', 'codeprem'],
   category: 'owner',
   isOwner: true,
-  run: async (client, m, args) => {
+  run: async (client, m, { args }) => {
     const token = crypto.randomBytes(4).toString('hex').toUpperCase()
-    
+    const expira = Date.now() + (30 * 24 * 60 * 60 * 1000) // 30 días en ms
+
     if (!global.db.data.premiumTokens) global.db.data.premiumTokens = []
     
     global.db.data.premiumTokens.push({
       token: token,
       status: 'available',
-      date: new Date().toISOString()
+      date: Date.now(),
+      expiration: expira
     })
 
     const txt = `*《 ✧ TOKEN BOT PREMIUM ✧ 》*\n\n` +
                 `> 🔑 Código: \`${token}\`\n` +
-                `> ⚠️ Al canjearlo, se creará una sesión en la carpeta /Premium.\n\n` +
+                `> 📅 Validez del Token: *30 Días*\n` +
+                `> ⚠️ Al conectar, el bot durará **1 mes** activo.\n\n` +
                 `*Canjear con:* \`.bepremium ${token}\``
     
     await m.reply(txt)
